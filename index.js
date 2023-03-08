0c1d641e35cdb2af95f7aff391fbea65210dc7af
Nkatha_Glory <kinyua6945@gmail.com>
2023-03-08 14:22:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your solution in this file!
+const details=["employee", "key", "value"]
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return details1
+}
