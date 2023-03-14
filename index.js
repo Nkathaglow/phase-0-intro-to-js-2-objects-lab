6fe666187d1acbc5f88d713f327c59fadc26108e
Nkatha_Glory <kinyua6945@gmail.com>
2023-03-14 16:25:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const details=["employee", "key", "value"]
+const employee = {
+    name : "Glory",
+    streetAddress: "11 Broadway"
+}
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return details1
+function updateEmployeeWithKeyAndValue(employee, key, value)  {
+   let employ = {...employee}
+   employ[key] = value
+   return employ
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   employee[key] = value
+   return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+   let newEmployee = {...employee}
+   delete newEmployee[key]
+   return newEmployee
+} 
+
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+   delete employee[key]
+   return employee
 }
